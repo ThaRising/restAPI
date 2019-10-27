d76bc61bd9edbca858ff7b622314b7830b194c23
ThaRising <kochbe,ber@gmail.com>
2019-10-27 19:35:04 +0100

    Provided connection to database cluster

---
+++ config/db.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+// Load environmental variables
+dotenv.config({ path: 'config.env' });
+
+// Connect to Database
+const connectDB = async () => {
+  const conn = await mongoose.connect(process.env.MONGO_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  });
+  console.log(`MongoDB Connected: ${conn.connection.host}`);
+};
+
+module.exports = connectDB;
