2f7ce3d6e54848850ddfa6c358aba656ea47f8c6
ThaRising <kochbe.ber@gmail.com>
2019-11-02 15:20:47 +0100

    Reworked schema

--- models/User.js
+++ models/User.js
@@ -4,44 +4,60 @@ const UserSchema = new mongoose.Schema(
   {
     username: {
       type: String,
-      required: [true, 'Please add a username'],
-      unique: false,
       trim: true,
-      maxlength: [20, 'Name may not be longer than 20 characters'],
-      match: [/^\S*$/, 'Please add a valid username']
+      required: [true, 'A username is required for signup.'],
+      minlength: [3, 'Name may not be shorter than 3 characters.'],
+      maxlength: [15, 'Name may not be longer than 15 characters.'],
+      match: [
+        /^([^_\W]*)$/i,
+        'Usernames may not contain non word characters and spaces.'
+      ]
     },
-    tag: {
-      type: Number,
-      unique: true
+    tag: Number,
+    slug: {
+      type: String,
+      unique: true,
+      index: true
     },
-    slug: String,
     email: {
       type: String,
-      index: true,
-      required: [true, 'Please add an email adress'],
       unique: true,
+      required: [true, 'Please add an email adress'],
       match: [
         /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i,
         'Please add a valid email adress'
       ]
     },
+    country: String,
     createdAt: {
       type: Date,
       default: Date.now
-    },
-    country: String
+    }
   },
   { autoIndex: false }
 );
 
+UserSchema.methods.generateTag = async function() {
+  const promise = await this.constructor
+    .find({ username: this.username })
+    .sort({ tag: -1 })
+    .limit(1)
+    .exec();
+  return promise.length ? Number(promise[0].tag) + 1 : Number(1000);
+};
+
 UserSchema.methods.generateSlug = function() {
   return this.username
+    .toString()
+    .toLowerCase()
     .replace(/\s+/g, '-')
     .replace(/[^\w\-]+/g, '')
-    .replace(/\-\-+/g, '-');
+    .replace(/\-\-+/g, '-')
+    .concat(`-${this.tag}`);
 };
 
 UserSchema.pre('save', async function() {
+  this.tag = await this.generateTag();
   this.slug = await this.generateSlug();
 });
 
