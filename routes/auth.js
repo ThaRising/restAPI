caca67f687d90cba3e7a2e42a3358581cf308203
ThaRising <kochbe.ber@gmail.com>
2019-11-11 21:24:15 +0100

    Removed auth endpoint as scheme is not RESTful

--- routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { signup } = require('../controllers/auth');
-
-const router = express.Router();
-
-router.route('/signup').post(signup);
-
-module.exports = router;
