1c349a29b3adf865549c995d0d6360f2daea3f02
ThaRising <kochbe.ber@gmail.com>
2019-11-12 20:55:02 +0100

    Removed depracated endpoint

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const errorHandler = require('./middleware/error');
-const connectDB = require('./config/db');
+const express = require("express");
+const dotenv = require("dotenv");
+const errorHandler = require("./middleware/error");
+const connectDB = require("./config/db");
 
 // Load environmental variables
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: "./config/config.env" });
 
 // Import routers
-const users = require('./routes/users');
-const auth = require('./routes/auth');
+const users = require("./routes/users");
 
 // Create router
 const app = express();
 app.use(express.json());
 
 // Mount Routers
-app.use('/api/v0/users', users);
-app.use('/api/v0/auth', auth);
+app.use("/api/v0/users", users);
 app.use(errorHandler);
 
 // Connect to database
@@ -26,13 +24,11 @@ const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
-  console.log(
-    `Server running in ${process.env.NODE_ENV} mode on Port ${process.env.PORT}`
-  )
+  console.log(`Server running in ${process.env.NODE_ENV} mode on Port ${PORT}`)
 );
 
 // Handle unhandled promise rejections
-process.on('unhandledRejection', (err, promise) => {
+process.on("unhandledRejection", (err, promise) => {
   console.log(`Error: ${err.message}`);
   // Close server and  exit process
   server.close(() => process.exit(1));
