4e4f3a49d9433d0f0200cdd9a3aba4c31e11fee5
ThaRising <kochbe.ber@gmail.com>
2019-11-18 00:16:54 +0100

    Reworked local Strategy

--- config/passport.js
+++ config/passport.js
@@ -1,24 +1,31 @@
-const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const User = require('../models/User');
 
-passport.use(new LocalStrategy({
-    usernameField: 'email'
-  },
-  function(username, password, done) {
-    User.findOne({ email: username }, function (err, user) {
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, {
-          message: 'User not found'
-        });
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, {
-          message: 'Password is wrong'
-        });
-      }
-      return done(null, user);
-    });
-  }
-));
+function initialize(passport) {
+	passport.use(
+		new LocalStrategy({ usernameField: 'email' }, function(email, password, done) {
+			User.findOne({ email: email }, function(err, user) {
+				if (err) {
+					return done(err);
+				}
+				if (!user) {
+					return done(null, false, {
+						message: 'User not found'
+					});
+				}
+				if (!user.validPassword(password)) {
+					return done(null, false, {
+						message: 'Password is wrong'
+					});
+				}
+				return done(null, user);
+			});
+		})
+	);
+	passport.serializeUser((user, done) => done(null, user._id));
+	passport.deserializeUser((id, done) => {
+		return done(null, User.findOne({ _id: id }));
+	});
+}
+
+module.exports = initialize;
