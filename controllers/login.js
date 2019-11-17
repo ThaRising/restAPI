1997ef48cbbf395b8e3ad7b60ea720f92ebb82e1
ThaRising <kochbe.ber@gmail.com>
2019-11-18 00:17:03 +0100

    Reworked login controller

--- controllers/login.js
+++ controllers/login.js
@@ -1,17 +1,31 @@
 const passport = require('passport');
-const User = require("../models/User")
+const User = require('../models/User');
+require('../config/passport')(passport);
 
 module.exports.login = function(req, res, next) {
-    passport.authenticate('local', { successRedirect: '/',
-    failureRedirect: '/login',
-    failureFlash: true }, function(err, user, info){
-      if (err) {
-        res.status(404).json(err);
-      }
-      if(user){
-        res.status(200).json(User.generateJwt());
-      } else {
-        res.status(401).json(info);
-      }
-    })(req, res);
-  };
+	try {
+		passport.authenticate(
+			'local',
+			{
+				successRedirect: '/',
+				failureRedirect: '/login',
+				failureFlash: true
+			},
+			function(err, user, info) {
+				if (err) {
+					res.status(404).json(err);
+				}
+				if (user) {
+					res.status(200).json(user.toAuthJSON());
+				} else {
+					res.status(401).json(info);
+				}
+			}
+		)(req, res, next);
+	} catch (error) {
+		res.status(400).json({
+			success: false,
+			error: error.message
+		});
+	}
+};
